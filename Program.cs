af765f5a47d51a184e7f0823cb4eef6662d8cd32
sebastiansoto1407 <sebastiansotoastete1407@gmail.com>
2025-08-08 09:37:58 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    class Usuario
+    {
+        public string Nombre { get; set; }
+
+        public Usuario(string nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+    class Movimiento
+    {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+
+        public Movimiento()
+        {
+            PosX = 0;
+            PosY = 0;
+        }
+    }
+    class Puntaje
+    {
+        public int Puntos { get; set; }
+
+        public Puntaje()
+        {
+            Puntos = 0;
+        }
+    }
+    class Personaje
+    {
+        public Usuario Usuario { get; set; }
+        public Movimiento Movimiento { get; set; }
+        public Puntaje Puntaje { get; set; }
+
+        public Personaje(string nombre)
+        {
+            Usuario = new Usuario(nombre);
+            Movimiento = new Movimiento();
+            Puntaje = new Puntaje();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos al juego, ingrese un nombre de usuario");
+            string nombre= Console.ReadLine();
+
+            Console.WriteLine("\nControles: W=arriba, S=abajo, A=izquierda, D=derecha, +=+10 puntos, Q= salir\n");
+            bool jugando = true;
+            
+            
+
+
+        }
+    }
+}
